5b1b24d98e0f400f41249a7578654a8353654a79
Prashant Kumar <prashant.aman9@gmail.com>
2021-10-30 12:46:26 +0530

    add routes and schema for dtoring notes

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,11 @@ const connectToMongo=require('./db');
 const port = 3000
 connectToMongo();
 
+
+//Availaible routes
+app.use('/api/auth',require('./routes/auth'));
+app.use('/api/notes',require('./routes/notes'));
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
