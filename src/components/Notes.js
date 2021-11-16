291f1877e529e27be2fe5ccda0d633c791acb1b6
Prashant Kumar <prashant.aman9@gmail.com>
2021-11-16 20:35:09 +0530

    add fontawesome icons in noteitem

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -8,7 +8,7 @@ const Notes = () => {
     <div className="row my-3">
       <h1>Your notes</h1>
       {context.notes.map((note) => {
-        return <Noteitem note={note}/>
+        return <Noteitem key={note._id} note={note}/>
       })}
     </div>
   );
