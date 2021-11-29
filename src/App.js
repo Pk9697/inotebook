6779ede1f057a17697590cf4fd02950921cb158f
Prashant Kumar <prashant.aman9@gmail.com>
2021-11-29 21:27:36 +0530

    add login component which receives auth token as json response from server

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
 import NoteState from "./context/notes/NoteState";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
 function App() {
   return (
     <>
@@ -13,6 +15,9 @@ function App() {
             <Routes>
               <Route exact path="/" element={<Home />} />
               <Route exact path="/about" element={<About />} />
+              <Route exact path="/login" element={<Login />} />
+              <Route exact path="/signup" element={<Signup />} />
+
             </Routes>
           </div>
         </Router>
